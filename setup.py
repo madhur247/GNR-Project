43d785df118ad0a9a555e172cf252755533c96b5
madhur247 <madhurthakar247@gmail.com>
2026-05-03 02:11:27 +0530

    Initail commit

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from transformers import AutoProcessor, AutoModelForImageTextToText
 import torch
+LOCAL_ONLY = False
 
 MODEL_PATH = "Qwen/Qwen2-VL-7B-Instruct"
 save_dir = "./qwen2_vl_offline_weights"
-LOCAL_ONLY = True
 
 processor = AutoProcessor.from_pretrained(
-    MODEL_PATH, backend="torchvision", local_files_only=LOCAL_ONLY,
+    MODEL_PATH, use_fast=True, local_files_only=LOCAL_ONLY,
 )
 
 dtype = torch.float16
@@ -16,4 +16,4 @@ model = AutoModelForImageTextToText.from_pretrained(
 )
 processor.save_pretrained(save_dir)
 model.save_pretrained(save_dir)
-print("Download complete. You can now use this folder for offline evaluation.")
+print("Download complete")
